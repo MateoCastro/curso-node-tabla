0b59174c7a156cf14d40eeb5404dc4cad749dd3e
Mateo Castro Muñoz <mateo@experimentality.co>
2024-07-29 21:01:34 -0500

    Primer commit.

---
+++ app.js
@@ -0,0 +1,10 @@
+const {crearArchivo} = require('./helpers/multiplicar');
+const argv = require('./config/yargs');
+
+require('colors');
+
+console.clear();
+
+crearArchivo(argv.base, argv.listar, argv.hasta)
+  .then(nombreArchivo => console.log(nombreArchivo.rainbow, 'creado'))
+  .catch(console.warn);
